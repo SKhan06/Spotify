bf38e7a6626ab09e5c4958c2579ae029c82f4226
Sohail <amdsohailalam@gmail.com>
2024-06-01 22:56:46 +0530

    fixes

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
   currFolder = folder;
-  let a = await fetch(`${folder}/`);
+  let a = await fetch(`https://bg1jls0t-3000.inc1.devtunnels.ms/${folder}/`);
   let response = await a.text();
   let div = document.createElement("div");
   div.innerHTML = response;
@@ -66,7 +66,7 @@ const playMusic = (track, pause = false) => {
 
 async function displayAlbums() {
   try {
-    let response = await fetch(`/songs/`);
+    let response = await fetch(`https://bg1jls0t-3000.inc1.devtunnels.ms/songs/`);
     if (!response.ok) {
       throw new Error(`Failed to fetch albums: ${response.statusText}`);
     }
@@ -84,7 +84,7 @@ async function displayAlbums() {
       if (e.href.includes("/songs")) {
         let folder = e.href.split("/").slice(-2)[0];
         try {
-          let infoResponse = await fetch(`/songs/${folder}/info.json`);
+          let infoResponse = await fetch(`https://bg1jls0t-3000.inc1.devtunnels.ms/songs/${folder}/info.json`);
           if (!infoResponse.ok) {
             console.error(`Info file not found for folder: ${folder}`);
             continue;
